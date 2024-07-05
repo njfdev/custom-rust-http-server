22615e84d7f08a2204305fc8604d633658096f19
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-05 15:49:18 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::net::TcpListener;
-use std::io::Write;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -9,12 +11,34 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                println!("accepted new connection");
-                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("write failed");
+                handle_request(&mut _stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+
+fn handle_request(stream: &mut TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let request: String = String::from_utf8_lossy(&buffer).to_string();
+    let request_endpoint = request.split_whitespace().nth(1).unwrap();
+
+    let status_code: &str;
+
+    match request_endpoint {
+        "/" => {
+            status_code = "200 OK";
+        }
+        _ => {
+            status_code = "404 Not Found";
+        }
+    }
+
+    stream.write(format!("HTTP/1.1 {}\r\n\r\n", status_code).as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
